73bb7b06d02859db68af23104069272ccb8141cc
Michael R. Lawrence <mike@michaellawrenceit.com>
2018-05-11 11:43:05 -0500

    add pine/rock64 config & crossdev-utilscripts

---
+++ scripts/ebuildr.sh
@@ -0,0 +1,19 @@
+%################## next 
+#!/bin/sh
+#ebuildr
+# builds world binaries when posible but dose not yet merge them..  thus if an emerge went bad it wont break system and or updates will go faster. 
+LIST=$(mktemp);
+
+emerge -puDN --color=n --columns --quiet=y world | awk '{print $2}' > ${LIST};
+
+for PACKAGE in $(cat ${LIST});
+do
+  printf "Building binary package for ${PACKAGE}... "
+  emerge -uN --quiet-build --quiet=y --buildpkgonly ${PACKAGE};
+  if [[ $? -eq 0 ]];
+  then
+    echo "ok";
+  else
+    echo "failed";
+  fi
+done
