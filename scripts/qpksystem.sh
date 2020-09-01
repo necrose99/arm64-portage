0539ebb5108e11de5c458c621a817ed89718b5f8
Michael L <mike@michaellawrenceit.com>
2020-09-01 12:12:37 -0500

    Update qpksystem.sh

--- scripts/qpksystem.sh
+++ scripts/qpksystem.sh
@@ -1,4 +1,5 @@
 #/bin/bash 
 #https://gentoo-user.gentoo.narkive.com/XsKNePuh/system-backup-with-quickpkg
+## runs quickpkg against any and all installed pkgs..  ie @system/world might perhaps miss...  due to meta pkgs.. 
  
 find /var/db/pkg/ -type d -mindepth 2 -exec quickpkg {} \;
